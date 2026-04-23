d896dc50c1980b69045d3a1277b7ddd86f779008
Juanfran514 <jeb419@inlumine.ual.es>
2026-04-23 17:23:11 +0200

    feat: Added finding methods in repository

--- backend/src/main/java/com/impaintor/feature/room/repository/RoomRepository.java
+++ backend/src/main/java/com/impaintor/feature/room/repository/RoomRepository.java
@@ -4,9 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.impaintor.feature.room.models.Room;
 import com.impaintor.feature.room.models.Room.GameState;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface RoomRepository extends JpaRepository<Room, String>{
+    Optional<Room> findByRoomCode(String roomCode);
+
     List<Room> findByGameState(GameState gameState);
+
+    boolean existsByRoomCode(String roomCode);
+
+    List<Room> findByMode(Room.Mode mode);
     
 }
