25494d39c03581f8b1559a0964ad03d87c194c79
ABEL RODRIGUEZ GUTIERREZ <arg331@inlumine.ual.es>
2026-05-02 13:52:32 +0200

    feat: Canva funcionando

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CanvasComponent } from './features/game/components/canvas/canvas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CanvasComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
