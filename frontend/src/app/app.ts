e10c34d64b53fee65306c2c0813974e150cdd86a
PÉREZ GONZÁLEZ JOSÉ ESTEBAN <jpg486@inlumine.ual.es>
2026-05-06 16:29:44 +0200

    feat: update angular configuration and refactor app component structure

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CanvasComponent } from './features/game/components/canvas/canvas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CanvasComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
